747ef97aa63cd156046f9111a68b0e923b5acf79
alueken <adam.lueken@d128.org>
2024-08-26 07:37:07 -0500

    Delete string_manipulation.py

--- string_manipulation.py
+++
@@ -1,8 +0,0 @@
-'''
-Write a function reverse_and_uppercase(input_str) that takes a string, reverses it, and converts it to uppercase.
-'''
-def reverse_and_uppercase(input_str):
-    # Your code here
-
-assert reverse_and_uppercase("hello") == "OLLEH"
-assert reverse_and_uppercase("DataStructures") == "SERUTCURTSATAD"
