a9ed8899038f9f164677157d9bcbba0436d651ab
alueken <adam.lueken@d128.org>
2024-08-26 07:35:48 -0500

    Create arithmetic_operations.py

---
+++ arithmetic_operations.py
@@ -0,0 +1,7 @@
+'''
+Write a function basic_operations(a, b) that returns a tuple containing the sum, difference, product, and quotient of a and b.
+'''
+def basic_operations(a, b):
+    # Your code here
+
+assert basic_operations(10, 2) == (12, 8, 20, 5)
